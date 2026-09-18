fb163168d4b1d84126b1bb27d705514581975ac0
Vibhor Gupta <vibhorg62@gmail.com>
2026-09-18 23:28:48 +0530

    Time: 0 ms (100%), Space: 8.6 MB (44.32%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,6 +1,19 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        return pow(x,n);
+        long long power = n;
+        if(power<0){
+            x=1/x;
+            power=-power;
+        }
+        double ans = 1.0;
+        while(power>0){
+            if(power%2==1){
+                ans*=x;
+            }
+            x*=x;
+            power/=2;
+        }
+        return ans;
     }
 };
